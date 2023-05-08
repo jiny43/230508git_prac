8eb8bc6bdc6c60c029a270cd2e21c79bea4bc7a4
jieun <je_eun2735@naver.com>
2023-05-08 10:16:13 +0900

    mianFeed.js충돌해결

--- src/mainFeed.js
+++ src/mainFeed.js
@@ -1,29 +1,25 @@
-import React from 'react';
-import AlarmForm from '../components/AlarmForm';
-import BookStagramTopLogo from '../components/BookStagramTopLogo';
-import Hamburger from '../components/Hamburger';
-<<<<<<< HEAD
-import NavBar from './NavBar';
-=======
-import NavBar from '../components/NavBar';
+import React from "react";
+import AlarmForm from "../components/AlarmForm";
+import BookStagramTopLogo from "../components/BookStagramTopLogo";
+import Hamburger from "../components/Hamburger";
+import NavBar from "../components/NavBar";
 import TopMenu from "../components/TopMenu";
->>>>>>> nowkwon97/issuetest
 
 const headerStyle = {
-  display: 'flex',
-}
+  display: "flex",
+};
 
 const mainFeed = () => {
   return (
     <div>
       <header>
-        <TopMenu/>
+        <TopMenu />
       </header>
 
       {/* main 컨텐츠 부분은 NavBar 컴포넌트에서 담당한다. */}
 
       <footer>
-        <NavBar/>
+        <NavBar />
       </footer>
     </div>
   );
